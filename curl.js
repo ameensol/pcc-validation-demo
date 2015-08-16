e3580af4e47247720690d21f48f1340bd6a7c324
ameen <ameensol@gmail.com>
2015-08-16 17:41:39 -0400

    update json

--- curl.js
+++ curl.js
@@ -1,14 +1,18 @@
 var fs = require('fs')
 var Scratch = require('scratch-api')
 
+/*
 Scratch.createUserSession('redleader', '5paceBAR', function(err, user) {
   if (err) throw new Error(err);
 
   console.log(user)
   fs.writeFileSync('./user.json', JSON.stringify(user))
+})
+*/
 
-  Scratch.getProject(73253520, function (err, project) {
-    console.log(project)
-    fs.writeFileSync('./project.json', JSON.stringify(project))
-  })
+// 73253520 - empty
+
+Scratch.getProject(72900730, function (err, project) {
+  console.log(project)
+  fs.writeFileSync('./project.json', JSON.stringify(project))
 })
