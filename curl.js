0d290198b1953e607382b0e875106d53e6cc7f3d
ameen <ameensol@gmail.com>
2015-08-16 16:55:29 -0400

    initial commit

---
+++ curl.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+var Scratch = require('scratch-api')
+
+Scratch.createUserSession('redleader', '5paceBAR', function(err, user) {
+  if (err) throw new Error(err);
+
+  console.log(user)
+  fs.writeFileSync('./user.json', user)
+
+  Scratch.getProject(73253520, function (err, project) {
+    console.log(project)
+    fs.writeFileSync('./project.json', project)
+  })
+})
